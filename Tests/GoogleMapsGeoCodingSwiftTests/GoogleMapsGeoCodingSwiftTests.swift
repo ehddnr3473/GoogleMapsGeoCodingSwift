0ea06c910497ab620b90404e0681f633fbe39087
Donguk Kim <ehddnr73@naver.com>
2023-03-21 08:33:19 +0900

    testcase 작성

--- Tests/GoogleMapsGeoCodingSwiftTests/GoogleMapsGeoCodingSwiftTests.swift
+++ Tests/GoogleMapsGeoCodingSwiftTests/GoogleMapsGeoCodingSwiftTests.swift
@@ -2,9 +2,27 @@ import XCTest
 @testable import GoogleMapsGeoCodingSwift
 
 final class GoogleMapsGeoCodingSwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    func testRequest() {
+        let networkService = DefaultGeoCodingNetworkService(key: "Type your API key")
+        let expectation = self.expectation(description: "network")
+        networkService.request(with: .init(address: "masan"), EndPoint.default) { result in
+            switch result {
+            case .success(let data):
+                print(String(data: data, encoding: .utf8)!)
+                expectation.fulfill()
+            case .failure(let error):
+                print(String(describing: error))
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    @available(swift 5.5)
+    @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+    func testAsyncRequest() async throws {
+        let networkService = DefaultGeoCodingNetworkService(key: "Type your API key")
+        let data = try await networkService.request(with: .init(address: "masan"), EndPoint.default)
+        print(String(data: data, encoding: .utf8)!)
     }
 }
